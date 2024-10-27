44cc7835d5a73ad1dbf69b6f2c8d4f351042b4bf
santhoshgit <santhoshkumar240503@gmail.com>
2024-10-27 12:01:10 +0530

    Update script.js and style.css with new features

--- script.js
+++ script.js
@@ -14,13 +14,3 @@ navToggleBtn.addEventListener("click", function () {
 
 
 
-/**
- * show go top btn when scroll window to 500px
- */
-
-const goTopBtn = document.querySelector("[data-go-top]");
-
-window.addEventListener("scroll", function () {
-  window.scrollY >= 500 ? goTopBtn.classList.add("active")
-    : goTopBtn.classList.remove("active");
-});
